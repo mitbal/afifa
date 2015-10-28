1091cb0edcc9299da9f321d824c72bd2dc213f80
Muhammad Iqbal Tawakal <itawakal@traveloka.com>
2015-10-28 16:21:13 +0700

    Initial commit for helper file one_hot

---
+++ one_hot.py
@@ -0,0 +1,52 @@
+# Perform one hot encoding
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+fin = open('../payment/train.csv', 'r')
+fout = open('../payment/train_onehot.csv', 'w')
+dataset = []
+symbols = set()
+
+header = fin.readline().split(',')
+num_features = len(header)-1
+num_num = 0
+
+labels_numeric = ''
+for line in fin.readlines():
+    tokens = line[:-1].split(',')
+    for token in tokens[:-1]:
+        if not is_number(token) and token != '':
+            symbols.add(token)
+    dataset.append(tokens[:-1])
+fin.close()
+
+row = dataset[1]
+labels_numeric = ''
+for (idx, col) in enumerate(row):
+    if is_number(col):
+        labels_numeric += header[idx]+','
+
+symbols = list(symbols)
+labels_symbols = ','.join(map(lambda x: 'c_'+x, symbols))
+header = labels_numeric+labels_symbols
+print header
+fout.write(header+'\n')
+for row in dataset:
+    sym_row = ['0']*len(symbols)
+    num_row = ''
+    for col in row:
+        if is_number(col):
+            num_row += col+','
+        else:
+            for (idx, sym) in enumerate(symbols):
+                if sym == col:
+                    sym_row[idx] = '1'
+    prow = num_row+','.join(sym_row)
+    print prow
+    fout.write(prow +'\n')
+fout.close()
