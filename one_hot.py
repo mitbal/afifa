92e516972bee14174653f6927091cecc0cf14d9a
Muhammad Iqbal Tawakal <itawakal@traveloka.com>
2015-10-29 15:33:06 +0700

    Added forgotten label column

--- one_hot.py
+++ one_hot.py
@@ -8,7 +8,6 @@ def is_number(s):
     except ValueError:
         return False
 
-print(len(sys.argv))
 if len(sys.argv) < 3:
     print 'The usage is one_hot.py <input_file> <output_file>'
     sys.exit(0)
@@ -21,6 +20,7 @@ symbols = set()
 header = fin.readline().split(',')
 num_features = len(header)-1
 num_num = 0
+labels = []
 
 labels_numeric = ''
 for line in fin.readlines():
@@ -28,6 +28,7 @@ for line in fin.readlines():
     for token in tokens[:-1]:
         if not is_number(token) and token != '':
             symbols.add(token)
+    labels.append(tokens[-1].rstrip())
     dataset.append(tokens[:-1])
 fin.close()
 
@@ -39,10 +40,10 @@ for (idx, col) in enumerate(row):
 
 symbols = list(symbols)
 labels_symbols = ','.join(map(lambda x: 'c_'+x, symbols))
-header = labels_numeric+labels_symbols
+header = labels_numeric+labels_symbols+',class'
 print header
 fout.write(header+'\n')
-for row in dataset:
+for (nrow,row) in enumerate(dataset):
     sym_row = ['0']*len(symbols)
     num_row = ''
     for col in row:
@@ -52,7 +53,7 @@ for row in dataset:
             for (idx, sym) in enumerate(symbols):
                 if sym == col:
                     sym_row[idx] = '1'
-    prow = num_row+','.join(sym_row)
+    prow = num_row+','.join(sym_row)+','+labels[nrow]
     print prow
     fout.write(prow +'\n')
 fout.close()
