144fc46e229115c7e568c56f536eccddfa58c781
Muhammad Iqbal Tawakal <itawakal@traveloka.com>
2015-11-04 13:13:42 +0700

    Fixed normalization script

--- norma.py
+++ norma.py
@@ -7,34 +7,39 @@ if len(sys.argv) < 3:
 	sys.exit(1)
 
 filename = sys.argv[1]
-numcol = sys.argv[2]
+numcol = int(sys.argv[2])
 fin = open(filename, 'r')
 
 dataset = []
 totalsum = 0
 count = 0
-for line in fin.readlines():
+header = fin.readline()
+for line in fin:
 	tokens = line.split(',')
 	dataset.append(tokens)
-
-	for idx, token in enumerate(tokens):
-		if idx == numcol:
-			totalsum += float(token)
-			count += 1
+	totalsum += float(tokens[numcol])
+	count += 1
 avg = totalsum / count
 stdev = 0
 
 for row in dataset:
 	for idx, col in enumerate(row):
 		if idx == numcol:
-			stdev += math.pow((col-avg), 2)
+			stdev += math.pow((int(col)-avg), 2)
 stdev = math.sqrt(stdev/count)
 
 for row in dataset:
 	for idx, col in enumerate(row):
 		if idx == numcol:
-			row[idx] = row[idx]/stdev
+			row[idx] = float(row[idx])/stdev
 
-fout = open(filename, 'w')
+fout = open('normed_'+filename, 'w')
+fout.write(header)
 for row in dataset:
-	fout.write(','.join(row))
+	fout.write(','.join(map(lambda x: str(x), row)))
+
+fin.close()
+fout.close()
+print 'mean', avg
+print 'std', stdev
+print 'selesai'
