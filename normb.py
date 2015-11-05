75edb2bf6b0e92edffaeaa0ca74defea1b78d666
Muhammad Iqbal Tawakal <itawakal@traveloka.com>
2015-11-05 12:03:39 +0700

    Added script to apply normalization to pre-calculated scale

---
+++ normb.py
@@ -0,0 +1,31 @@
+# Apply normalization using pre-calculated mean and stdev
+import sys
+
+if len(sys.argv) < 3:
+	print 'usage: <filename> <norm_file>'
+
+filename = sys.argv[1]
+norm_file = sys.argv[2]
+
+fin = open(filename, 'r')
+fnorm = open(norm_file, 'r')
+fout = open(filename+'_n', 'w')
+
+norms = []
+header = fnorm.readline()
+for line in fnorm:
+	tokens = line.split(',')
+	norms.append([int(tokens[0]), float(tokens[1]), float(tokens[2])])
+
+header = fin.readline()
+fout.write(header)
+for line in fin:
+	tokens = line.split(',')
+	for norm in norms:
+		idx = norm[0]
+		tokens[idx] = str((float(tokens[idx])-norm[1]) / norm[2])
+	fout.write(','.join(tokens))
+
+fin.close()
+fnorm.close()
+fout.close()
