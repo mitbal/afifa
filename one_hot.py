96eabeef148791050473aaf995c7ac13e81632ed
Muhammad Iqbal Tawakal <itawakal@traveloka.com>
2015-11-06 16:37:17 +0700

    Changed all script's mode to become executable

--- one_hot.py
+++ one_hot.py
@@ -1,65 +1,11 @@
 # Perform one hot encoding
 import sys
+from subprocess import call
 
-def is_number(s):
-    try:
-        float(s)
-        return True
-    except ValueError:
-        return False
-
-if len(sys.argv) < 3:
-    print 'The usage is one_hot.py <input_file> <output_file>'
+if len(sys.argv) < 4:
+    print 'The usage is one_hot.py <input_file> <output_file> <header_file>'
     sys.exit(0)
 
-fin = open(sys.argv[1], 'r')
-fout = open(sys.argv[2], 'w')
-fheader = open('header.txt', 'w')
-dataset = []
-symbols = set()
+call(['python', '/home/traveloka/workspace/mit_utils/onehot_header.py', sys.argv[1], sys.argv[3]])
 
-header = fin.readline().split(',')
-num_features = len(header)-1
-num_num = 0
-labels = []
-
-labels_numeric = ''
-for line in fin.readlines():
-    tokens = line[:-1].split(',')
-    for (ncol,token) in enumerate(tokens[:-1]):
-        if not is_number(token) and token != '':
-            symbols.add(header[ncol]+'_'+token)
-    labels.append(tokens[-1].rstrip())
-    dataset.append(tokens[:-1])
-fin.close()
-
-row = dataset[1]
-labels_numeric = ''
-for (idx, col) in enumerate(row):
-    if is_number(col):
-        labels_numeric += header[idx]+','
-
-symbols = sorted(list(symbols))
-labels_symbols = ','.join(map(lambda x: 'c_'+x, symbols))
-outheader = labels_numeric+labels_symbols+',class'
-print outheader
-fheader.write(outheader+'\n')
-fheader.close()
-fout.write(outheader+'\n')
-for (nrow,row) in enumerate(dataset):
-    sym_row = ['0']*len(symbols)
-    num_row = ''
-    for (ncol,col) in enumerate(row):
-        if is_number(col):
-            num_row += col+','
-        else:
-            for (idx, sym) in enumerate(symbols):
-                #print sym, header[ncol]+'_'+col
-                if sym == header[ncol]+'_'+col:
-                    sym_row[idx] = '1'
-    prow = num_row+','.join(sym_row)+','+labels[nrow]
-    if nrow % 10000 == 0:
-        # print prow
-        print str(nrow)+'/'+str(len(dataset))
-    fout.write(prow +'\n')
-fout.close()
+call(['python', '/home/traveloka/workspace/mit_utils/onehot_apply.py', sys.argv[1], sys.argv[2], sys.argv[3], 'csv'])
