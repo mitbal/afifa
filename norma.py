6910dd142eac826bd2b5f73beff16c0a34beb167
Muhammad Iqbal Tawakal <itawakal@traveloka.com>
2015-11-06 10:48:51 +0700

    Fixed norma.py syntax error

--- norma.py
+++ norma.py
@@ -31,14 +31,14 @@ header = fin.readline()
 for line in fin:
 	tokens = line.split(',')
 	for c in cols:
-		stdev[c] += math.pow(float(row[c])-avg[c], 2)
+		stdev[c] += math.pow(float(tokens[c])-avg[c], 2)
 std = map(lambda x: math.sqrt(x/count), stdev)
 
 # Write mean and standard deviation to file
 fout = open(norm_file, 'w')
 fout.write('column,mean,std\n')
 for c in cols:
-	fout.write(','.join(lambda x: str(x), [c, avg[c], std[c]]))
+	fout.write(','.join(map(lambda x: str(x), [c, avg[c], std[c]])))
 
 fin.close()
 fout.close()
