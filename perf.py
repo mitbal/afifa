29f22ae3121cf7a44ec57e6b3268a96770527049
Muhammad Iqbal Tawakal <itawakal@traveloka.com>
2015-11-20 10:58:25 +0700

    Modified perf.py to be able to process labels with custom value

--- perf.py
+++ perf.py
@@ -7,43 +7,48 @@ import sys
 
 epsilon = 1e-10
 
-if len(sys.argv) < 4:
-	print 'usage: <labels.txt> <prediction.txt> <number_of_class>'
+if len(sys.argv) < 5:
+	print 'usage: <labels.txt> <prediction.txt> <class label 1> <class label 2>...'
 	sys.exit(1)
 
 f1 = open(sys.argv[1], 'r')
 f2 = open(sys.argv[2], 'r')
-nc = int(sys.argv[3])
+# nc = int(sys.argv[3])
+labels = sys.argv[3:]
 
-label = []
 prediction = []
-conf_matrix = [[0]*nc for x in xrange(nc)]
+conf_matrix = {}
+for i in labels:
+	conf_matrix[i] = {}
+	for j in labels:
+		conf_matrix[i][j] = 0
+# conf_matrix = [[0]*nc for x in xrange(nc)]
 
 for line1 in f1.readlines():
 	line2 = f2.readline()
-	i = int(float(line1[:-1]))-1
-	j = int(float(line2[:-1]))-1
+	# i = int(float(line1[:-1]))-1
+	# j = int(float(line2[:-1]))-1
+	i = line1[:-1]
+	j = line2[:-1]
 	conf_matrix[i][j] += 1
 
 print
 header = ''
-for i in xrange(nc):
-	if i != nc-1:
-		header += str(i+1).rjust(12)
-	else:
-		header += str(i+1).rjust(11)
+for label in labels:
+	header += label.rjust(12)
 print header
-for i in xrange(nc):
-	row = str(i+1)+' '
-	for j in xrange(nc):
+
+for i in labels:
+	row = i+' '
+	for j in labels:
 		row += str(conf_matrix[i][j]).rjust(10) +' '
 	print row
 
 # Calculate accuracy
 acc = 0
 total = 0
-for i in xrange(nc):
-	for j in xrange(nc):
+for i in labels:
+	for j in labels:
 		if i==j:
 			acc += conf_matrix[i][j]
 		total += conf_matrix[i][j]
@@ -52,14 +57,14 @@ print
 print 'acc', str((float(acc)/total)*100)+'%', '('+str(acc)+'/'+str(total)+')'
 
 # Precision and recall for each classes
-precs = [0]*nc
-recs = [0]*nc
-for i in xrange(nc):
+precs = {}
+recs = {}
+for i in labels:
 	precs[i] = conf_matrix[i][i]
 	recs[i] = conf_matrix[i][i]
 	prec_denom = 0
 	rec_denom = 0
-	for j in xrange(nc):
+	for j in labels:
 		prec_denom += conf_matrix[j][i]
 		rec_denom += conf_matrix[i][j]
 	if(prec_denom != 0):
@@ -69,16 +74,16 @@ for i in xrange(nc):
 
 print
 print 'precision'.rjust(23), 'recall'.rjust(15)
-for i in xrange(nc):
-	print 'class', (i+1), str(precs[i]).rjust(15), str(recs[i]).rjust(15)
+for i in labels:
+	print 'class', i, str(precs[i]).rjust(15), str(recs[i]).rjust(15)
 
 # F measure
-fmeasure = [0]*nc
+fmeasure = {}
 print
 print 'f measure'
-for i in xrange(nc):
+for i in labels:
 	if (precs[i]+recs[i]) > epsilon:
 		fmeasure[i] = (2*precs[i]*recs[i]) / (precs[i]+recs[i])
 	else:
 		fmeasure[i] = 0
-	print 'class', (i+1), ':', fmeasure[i]
+	print 'class', i, ':', fmeasure[i]
