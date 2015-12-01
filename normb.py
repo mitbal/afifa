ed2524ea447f1c0c3df73ea14185451e294e49b3
Muhammad Iqbal Tawakal <itawakal@traveloka.com>
2015-12-01 13:45:49 +0700

    Fixed runtime error

--- normb.py
+++ normb.py
@@ -2,13 +2,14 @@
 
 # Apply normalization using pre-calculated mean and stdev
 import sys
+import math
 
 if len(sys.argv) < 3:
     print 'usage: <input_file> <output_file> <scale_file>'
 
 input_file = sys.argv[1]
 output_file = sys.argv[2]
-scale_file = sys.argv[2]
+scale_file = sys.argv[3]
 
 fin = open(input_file, 'r')
 fnorm = open(scale_file, 'r')
@@ -18,7 +19,10 @@ norms = []
 header = fnorm.readline()
 for line in fnorm:
     tokens = line.split(',')
-    norms.append([int(tokens[0]), float(tokens[1]), float(tokens[2])])
+    mean = float(tokens[1])
+    std = float(tokens[2])
+    if not (math.isnan(mean) or math.isnan(std) or mean < 1e-10 or std < 1e-10):
+        norms.append([int(tokens[0]), float(tokens[1]), float(tokens[2])])
 
 header = fin.readline()
 fout.write(header)
@@ -26,10 +30,7 @@ for line in fin:
     tokens = line.split(',')
     for norm in norms:
         idx = norm[0]
-        if norm[2] > 1e-10:
-            tokens[idx] = str((float(tokens[idx])-norm[1]) / norm[2])
-        else:
-            tokens[idx] = str((float(tokens[idx])-norm[1]))
+        tokens[idx] = str((float(tokens[idx])-norm[1]) / norm[2])
     fout.write(','.join(tokens))
 
 fin.close()
