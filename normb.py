46c9b0ce2c696082051b6c9fe5e9f3a31381c530
Muhammad Iqbal Tawakal <itawakal@traveloka.com>
2015-12-01 12:46:57 +0700

    Cleaned up normb.py

--- normb.py
+++ normb.py
@@ -4,32 +4,33 @@
 import sys
 
 if len(sys.argv) < 3:
-	print 'usage: <filename> <norm_file>'
+    print 'usage: <input_file> <output_file> <scale_file>'
 
-filename = sys.argv[1]
-norm_file = sys.argv[2]
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+scale_file = sys.argv[2]
 
-fin = open(filename, 'r')
-fnorm = open(norm_file, 'r')
-fout = open(filename+'_n', 'w')
+fin = open(input_file, 'r')
+fnorm = open(scale_file, 'r')
+fout = open(output_file, 'w')
 
 norms = []
 header = fnorm.readline()
 for line in fnorm:
-	tokens = line.split(',')
-	norms.append([int(tokens[0]), float(tokens[1]), float(tokens[2])])
+    tokens = line.split(',')
+    norms.append([int(tokens[0]), float(tokens[1]), float(tokens[2])])
 
 header = fin.readline()
 fout.write(header)
 for line in fin:
-	tokens = line.split(',')
-	for norm in norms:
-		idx = norm[0]
+    tokens = line.split(',')
+    for norm in norms:
+        idx = norm[0]
         if norm[2] > 1e-10:
-		  tokens[idx] = str((float(tokens[idx])-norm[1]) / norm[2])
+            tokens[idx] = str((float(tokens[idx])-norm[1]) / norm[2])
         else:
             tokens[idx] = str((float(tokens[idx])-norm[1]))
-	fout.write(','.join(tokens))
+    fout.write(','.join(tokens))
 
 fin.close()
 fnorm.close()
