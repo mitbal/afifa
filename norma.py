9857c00d66c3ba89118f22200222d3c44348b3e7
Muhammad Iqbal Tawakal <itawakal@traveloka.com>
2015-12-01 12:31:17 +0700

    Renamed variables for clearer meaning

--- norma.py
+++ norma.py
@@ -5,12 +5,12 @@ import sys
 import math
 
 if len(sys.argv) < 4:
-	print 'usage: <filename> <column_number> <norm_scale>'
+	print 'usage: <input_file> <scale_file> <column_numbers>'
 	sys.exit(1)
 
-filename = sys.argv[1]
-columns = sys.argv[2].split(',')
-norm_file = sys.argv[3]
+input_file = sys.argv[1]
+scale_file = sys.argv[2]
+columns = sys.argv[3].split(',')
 
 # parse column number
 cols = []
@@ -24,7 +24,7 @@ for col in columns:
 # Calculate average
 count = 0
 totalsum = {}
-fin = open(filename, 'r')
+fin = open(input_file, 'r')
 header = fin.readline()
 
 for line in fin:
@@ -53,7 +53,7 @@ for c in stdev:
 	std[c] = math.sqrt(stdev[c]/count)
 
 # Write mean and standard deviation to file
-fout = open(norm_file, 'w')
+fout = open(scale_file, 'w')
 fout.write('column,mean,std\n')
 for c in cols:
 	fout.write(','.join(map(lambda x: str(x), [c, avg[c], std[c]]))+'\n')
